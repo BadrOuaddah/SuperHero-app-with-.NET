ace5410f3b73d9ac75b78e2c255868c8faeb9c9a
Badr Ouaddah <Badr.Ouaddah.07@gmail.com>
2023-08-19 12:33:33 +0100

    Add Entity Framework Core 6

--- SuperHero-API/SuperHero-API/Data/DataContext.cs
+++ SuperHero-API/SuperHero-API/Data/DataContext.cs
@@ -4,5 +4,7 @@ namespace SuperHero_API.Data
 {
     public class DataContext : DbContext
     {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<SuperHero> SuperHeroes => Set<SuperHero>();
     }
 }
