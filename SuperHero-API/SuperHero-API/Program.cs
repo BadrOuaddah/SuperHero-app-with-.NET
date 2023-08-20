c55ebfc87a6b7e4444f6eaec702e8d76a1154695
Badr Ouaddah <Badr.Ouaddah.07@gmail.com>
2023-08-20 12:09:54 +0100

    Register the DataContext in Program.cs

--- SuperHero-API/SuperHero-API/Program.cs
+++ SuperHero-API/SuperHero-API/Program.cs
@@ -1,9 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using SuperHero_API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddCors(options => options.AddPolicy(name: "SuperHeroOrigins", policy =>
