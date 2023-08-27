f45b5817131bf9b796fcdb30c0bc248408067306
Badr Ouaddah <Badr.Ouaddah.07@gmail.com>
2023-08-27 15:02:39 +0100

    Implement Web Service Calls on the Client

--- SuperHero-UI/src/app/services/super-hero.service.ts
+++ SuperHero-UI/src/app/services/super-hero.service.ts
@@ -14,8 +14,18 @@ export class SuperHeroService {
   constructor(private http: HttpClient) { }
 
   public getSuperHeroes(): Observable<SuperHero[]> {
-
-
     return this.http.get<SuperHero[]>(`${environment.apiUrl}/${this.url}`);
+  }
+
+  public updateSuperHeroes(hero : SuperHero): Observable<SuperHero[]> {
+    return this.http.put<SuperHero[]>(`${environment.apiUrl}/${this.url}`,hero);
+  }
+
+  public createSuperHeroes(hero : SuperHero): Observable<SuperHero[]> {
+    return this.http.post<SuperHero[]>(`${environment.apiUrl}/${this.url}`,hero);
+  }
+
+  public deleteSuperHeroes(hero : SuperHero): Observable<SuperHero[]> {
+    return this.http.delete<SuperHero[]>(`${environment.apiUrl}/${this.url}/${hero.id}`);
   }
 }
